90a8bf7a4446665c3713d6da78e937af9511ce8e
Marco Tettamanti <marcotettamanti@outlook.com.br>
2023-02-26 16:47:02 -0300

    email service

---
+++ backend/src/main/java/com/dev/backend/service/EmailService.java
@@ -0,0 +1,34 @@
+package com.dev.backend.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class EmailService{
+  @Autowired
+  private JavaMailSender javaMailSender;
+
+  @Value("$(spring.mail.username)")
+  private String sender;
+    
+  public String sendEmailText(String emailClient, String emailTitle, String emailMessage){
+
+    try {
+      SimpleMailMessage simpleMailMessage = new SimpleMailMessage();
+      simpleMailMessage.setFrom(sender);
+      simpleMailMessage.setTo(emailMessage);
+      simpleMailMessage.setSubject(emailTitle);
+      simpleMailMessage.setText(emailTitle);
+      javaMailSender.send(simpleMailMessage);
+
+      return "Email enviado";
+    } catch (Exception e) {
+        return "Erro ao enviar o email";
+    }
+
+  }
+}
--- backend/src/main/java/com/dev/backend/service/UserClientService.java
+++ backend/src/main/java/com/dev/backend/service/UserClientService.java
@@ -18,16 +18,18 @@ public class UserClientService {
   @Autowired
   private PermissionUserService permissionUserService;
 
+  @Autowired
+  private EmailService emailService;
+
   //Salvar User
   public User insert(UserClientRequestDTO obj){
     User user = new UserClientRequestDTO().convertUser(obj); //Transformando objeto UserClient(DTO) em User
     user.setCreatDate(Instant.now());
     User newUser = repository.saveAndFlush(user); 
     permissionUserService.bindUserPermissionClient(newUser); //Associar as permissões ao User
+
+    
+    emailService.sendEmailText(newUser.getEmail(), "Cadastro na Loja Virtual", "Registro na loja foi realizado com sucesso. Em breve você receberá a senha de acesso ao email");
     return newUser;
   }
-
-
-
-
 }
